55c1fd15b595794375c11cb4ad3f20098d5ffb5f
Vasya207 <khymynetsvasya@gmail.com>
2023-02-10 20:18:12 +0200

    Modified powerups system

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -2,20 +2,7 @@ using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
     [SerializeField] int healAmount = 25;
-
-    Rigidbody2D myRigidbody;
-
-    void Awake()
-    {
-        myRigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    void Start()
-    { 
-        myRigidbody.velocity = new Vector2(0, -moveSpeed);
-    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/Scripts/PowerupsSpawner.cs
+++ Assets/Scripts/PowerupsSpawner.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 
 public class PowerupsSpawner : MonoBehaviour
 {
+    [Header("Powerups Prefabs")]
     [SerializeField] GameObject powerup;
+
+    [Header("Spawn Time Settings")]
     [SerializeField] float timeBeforeFirstSpawn = 7.5f;
     [SerializeField] float timeBetweenSpawns = 15f;
     [SerializeField] float spawnTimeVariance = 5f;
     [SerializeField] float minimumSpawnTime = 7.5f;
 
+    [Header("Powerups Settings")]
+    [SerializeField] float powerupMoveSpeed = 5f;
+    [SerializeField] float powerupLifeTime = 7f;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    Rigidbody2D powerupRigidbody;
+
     void Start()
     {
+        SetBoundariesForSpawn();
         StartCoroutine(SpawnPowerups());
     }
 
@@ -19,12 +31,30 @@ public class PowerupsSpawner : MonoBehaviour
         yield return new WaitForSeconds(timeBeforeFirstSpawn);
         while (true)
         {
-            int randomXPos = Random.Range(-4, 5);
+            float randomXPos = Random.Range(minBounds.x, maxBounds.x);
             Vector3 newSpawnPosition = new(randomXPos, transform.position.y, transform.position.z);
 
-            Instantiate(powerup, newSpawnPosition, Quaternion.identity, transform);
+            GameObject powerupInstance = 
+                Instantiate(powerup, newSpawnPosition, Quaternion.identity, transform);
+
+            powerupRigidbody = powerupInstance.GetComponent<Rigidbody2D>();
+            if(powerupRigidbody != null)
+            {
+                powerupRigidbody.velocity = -transform.up * powerupMoveSpeed;
+            }
+
+            Destroy(powerupInstance, powerupLifeTime);
             yield return new WaitForSeconds(GetRandomSpawnTime());
         }
+    }
+
+    void SetBoundariesForSpawn()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
+        minBounds.x++; maxBounds.x--;
     }
 
     float GetRandomSpawnTime()
